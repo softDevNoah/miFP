e04e6c4a485c2531471e64282a9db59e0ae94660
Noah <nbqnoah@gmail.com>
2025-09-11 19:59:07 +0200

    primerosDias
    Creación del repositorio y primera copia.

---
+++ practicasDia11/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module practicasDia11 {
+}
---
+++ practicasDia11/src/programApuntes2223/Valorabs.java
@@ -0,0 +1,18 @@
+package programApuntes2223;
+
+
+
+import java.util.Scanner;
+
+public class Valorabs {
+
+	public static void main(String[] args) {
+		Scanner	teclado = new Scanner(System.in);
+		String	entrada;
+		int		num;
+			
+		
+		teclado.close();
+	}
+
+}
