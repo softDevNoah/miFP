52e3e22a5ab7b4729323b9e12416c49c3bc9eab1
Noah <nbqnoah@gmail.com>
2025-09-18 15:56:56 +0200

    create modulo+MainClss entrega 3 UD2_5

---
+++ ejercicioUD2_5_Noah/src/ejercicioUD2_5_3_Noah/Main.java
@@ -0,0 +1,15 @@
+package ejercicioUD2_5_3_Noah;
+
+import java.util.Scanner;
+
+/*Entrega 3 de 4 (5 contando con los ejercicios extra) de los ejercicios del Tema 2.
+ * En este paquete se desarrollan los ejercicios del 8 al 12*/
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
