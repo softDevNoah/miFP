1ee996970a714e10855cd6b74339ca37c9935dc8
Noah <nbqnoah@gmail.com>
2025-09-20 19:38:04 +0200

    create entrega 4/UD2_5
    class Main, metodo main, y enunciados de los ejercicios 13 y 14

---
+++ ejercicioUD2_5_Noah/src/ejercicioUD2_5_4_Noah/Main.java
@@ -0,0 +1,63 @@
+package ejercicioUD2_5_4_Noah;
+
+import java.util.Scanner;
+
+/*Entrega 4 de 4 (5 contando con los ejercicios extra) de los ejercicios del Tema 2.
+ * En este paquete se desarrollan los ejercicios 13 y 14*/
+
+public class Main {
+	
+	private static Scanner	teclado = new Scanner(System.in);
+	private static String	entrada;
+	private static String	numEjercicio;
+	private static int		num;
+	
+	public static void main(String[] args) {
+		
+		int opt;
+		
+		System.out.println("Escribe 13 o 14 (o 0 para salir).");
+		
+		numEjercicio = teclado.nextLine();
+		opt = Integer.parseInt(numEjercicio);
+		
+		while (opt !=0) {
+			switch (opt) {
+				case 13:
+					ejercicio13();
+					break;
+				case 14:
+					ejercicio14();
+					break;
+			}
+			if (opt != 0) {
+				System.out.println("-------------------------------------------------------------------------------------");
+				System.out.println("Escribe 13 o 14 (o 0 para salir).");
+				numEjercicio = teclado.nextLine();
+				opt = Integer.parseInt(numEjercicio);
+			}
+		}
+	}
+	
+	private static void ejercicio13() {
+		/*13. En un banco se procesan datos de las cuentas corrientes de sus clientes.
+		 	De cada cuenta corriente se conoce: número de cuenta y saldo actual.
+		  	El ingreso de datos debe finalizar al ingresar un valor negativo en el número de cuenta.
+			Se pide confeccionar un programa que lea los datos de las cuentas corrientes e informe:
+			a)De cada cuenta: número de cuenta y estado de la cuenta según su saldo,
+			sabiendo que Estado de la cuenta puede tener los siguientes valores:
+				● 'Acreedor' si el saldo es >0.
+				● 'Deudor' si el saldo es <0.
+				● 'Nulo' si el saldo es =0.
+			b) La suma total de los saldos acreedores.*/
+		
+		
+		
+	}
+	private static void ejercicio14() {
+		/*14. Escribir un programa que lea 10 notas de alumnos y nos informe cuántos tienen notas
+		 * mayores o iguales a 7 y cuántos menores.*/
+		
+		
+	}
+}
