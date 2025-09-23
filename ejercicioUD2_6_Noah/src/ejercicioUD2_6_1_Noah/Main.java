ccbf7e3ff58189bc7c3de015e855a592074db82e
Noah <nbqnoah@gmail.com>
2025-09-23 17:56:00 +0200

    done: ejercicio1() y ejercicio2()
    falta comentar, depurar e indentar si no estuviera ya bien indentado

--- ejercicioUD2_6_Noah/src/ejercicioUD2_6_1_Noah/Main.java
+++ ejercicioUD2_6_Noah/src/ejercicioUD2_6_1_Noah/Main.java
@@ -10,13 +10,13 @@ public class Main {
 	private static Scanner	teclado = new Scanner(System.in);
 	private static String	entrada;
 	private static String	numEjercicio;
-	private static int		num;
+	//private static int		num;
 	
 	public static void main(String[] args) {
 		
 		int opt;
 		
-		System.out.println("Escribe un nº del 1 al 5 (o 0 para salir).");
+		System.out.println("Escribe un nº del 1 al 6 (o 0 para salir).");
 		
 		numEjercicio = teclado.nextLine();
 		opt = Integer.parseInt(numEjercicio);
@@ -44,7 +44,7 @@ public class Main {
 			}
 			if (opt != 0) {
 				System.out.println("-------------------------------------------------------------------------------------");
-				System.out.println("Escribe un nº del 1 al 5 (o 0 para salir).");
+				System.out.println("Escribe un nº del 1 al 6 (o 0 para salir).");
 				numEjercicio = teclado.nextLine();
 				opt = Integer.parseInt(numEjercicio);
 			}
@@ -54,12 +54,53 @@ public class Main {
 
 	private static void ejercicio1() {
 		
+		int		stringLen = 0;
+		char	charInIndex;
 		
+		do {
+			System.out.println("Introduzca el texto que quiera: ");
+			
+			entrada = teclado.nextLine();
+			
+		} while (entrada == "");
+		
+		System.out.println("Calculando la longitud del texto introducido...:");
+		stringLen = entrada.length();
+		System.out.println("\tLa longitud de tu texto es de " + stringLen + " caracteres.");
+		
+		System.out.println("Buscando el carácter en la posición 7 del texto...:");
+		if (stringLen >= 8) {
+			charInIndex = entrada.charAt(7);
+			System.out.println("\tEl carácter que se encuentra en la posición 7 de tu String es \'" + charInIndex + "\'");
+		}
+		else
+			System.out.println("\tTu texto no es tan largo para ver qué caracter hay en la posición 7.");
+		
+		System.out.println("Buscando una \'x\' en el texto...:");
+		if (entrada.indexOf('x') != -1) {
+			System.out.println("\tLa letra \'x\' aparece en la posición " + entrada.indexOf('x') + " de tu texto.");
+		}
+		else
+			System.out.println("\tCarácter no encontrado");
+		System.out.println("Cambiando el texto original todo a mayúsculas...:");
+		System.out.println(entrada.toUpperCase());
 	}
 	
 	private static void ejercicio2() {
 		
+		int		contador = 0;
 		
+		System.out.println("Introduzca el texto que quiera: ");
+		entrada = teclado.nextLine();
+		
+		while (entrada.indexOf('a') != -1) {
+			contador++;
+			entrada = entrada.substring(entrada.indexOf('a') + 1, entrada.length());
+		}
+		if (contador > 0)
+			System.out.println("Hay " + contador + " letras \'a\' en el texto introducido.");
+		else
+			System.out.println("No había ninguna \'a\' en el texto introducido.");
 	}
 	
 	private static void ejercicio3() {
